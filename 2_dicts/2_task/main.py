765b9661fceb506c5178eebd1e898e20f205746c
Assppex <joom361@gmail.com>
2024-09-17 16:02:14 +0300

    all dict tasks done

--- 2_dicts/2_task/main.py
+++ 2_dicts/2_task/main.py
@@ -1,3 +1,5 @@
+import re
+
 def top_10_most_common_words(text: str) -> dict[str, int]:
     """Функция возвращает топ 10 слов, встречающихся в тексте.
 
@@ -7,5 +9,24 @@ def top_10_most_common_words(text: str) -> dict[str, int]:
     Returns:
         словарь типа {слово: количество вхождений}
     """
+    text = text.lower()
+    
+    words = re.findall(r'\b[а-яА-Я]{3,}\b', text)
     most_common = {}
+    
+    for word in words:
+        pattern = r'\b' + str(word) + r'\b'
+        most_common[word] = len(re.findall(pattern, text))
+    
+    
+    # print(most_common.items())
+    
+    most_common = dict(sorted(
+        most_common.items(),
+        key = lambda x: (-x[1],x[0]),
+        #reverse=True -x[1] - по значению идем по убыванию (приоритетный признак), x[0]-в случае равенства идем по возрастанию в алфавитном порядке (т.е. нам надо чтобы в случае возникновения твбг = x и чеёж = x ---> {'твбг':x, 'чеёж':x}), в случае если проставить reverse = True будет наоборот, потому что reverse применится к обеим сортировкам
+        )[0:10])
+
+    print(most_common)
+    
     return most_common
--- 2_dicts/3_task/main.py
+++ 2_dicts/3_task/main.py
@@ -1,3 +1,5 @@
+import re
+
 def format_phone(phone_number: str) -> dict[str, int]:
     """Функция возвращает отформатированный телефон.
 
@@ -7,6 +9,18 @@ def format_phone(phone_number: str) -> dict[str, int]:
     Returns:
         отформатированный телефон
     """
-    formatted_phone_number = ""
-
+    
+    formatted_phone_number = "".join(re.findall(r'\d+', phone_number))
+    
+    if formatted_phone_number.startswith('9') and len(formatted_phone_number) == 10:
+        
+        formatted_phone_number = '8 (9' + formatted_phone_number[1:3] + ') ' + formatted_phone_number[3:6] + '-' + formatted_phone_number[6:8] + '-' + formatted_phone_number[8:10]
+        
+    elif (formatted_phone_number.startswith('8') or formatted_phone_number.startswith('7')) and len(formatted_phone_number) == 11:
+        
+        formatted_phone_number = '8 (9' + formatted_phone_number[2:4] + ') ' + formatted_phone_number[4:7] + '-' + formatted_phone_number[7:9] + '-' + formatted_phone_number[9:11]
+        
+    else:
+        return formatted_phone_number
+        
     return formatted_phone_number
