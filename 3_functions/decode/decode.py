1764ea9b08ee9c15b16f0f6b891409528ec5d118
Assppex <joom361@gmail.com>
2024-09-17 21:18:58 +0300

    function tasks done

--- 3_functions/decode/decode.py
+++ 3_functions/decode/decode.py
@@ -1,4 +1,24 @@
+import numpy as np
 def decode_numbers(numbers: str) -> str | None:
+    letters = np.array([['а', 'б', 'в', 'г'], ['д', 'е', 'ж', 'з'], ['и', 'й', 'к', 'л'], ['м','н','о','п'], ['р','с','т','у'], ['ф','х','ц','ч'], ['ш','щ','ъ','ы'], ['ь','э','ю','я']])
+    special_symbols = np.array(['.', ',', '?', '!', ':', ';'])
     """Пишите ваш код здесь."""
     result = ""
+    splitted_nums = numbers.split(' ')
+    
+    for letter in splitted_nums:
+        if(letter.count(letter[0]) == len(letter)):
+            if int(letter[0]) - 2 >= 0 and  len(letter) <= 4:
+                result += letters[int(letter[0]) - 2][len(letter) - 1]
+            elif int(letter[0]) == 1 and  len(letter) <= 6:
+                result += special_symbols[len(letter) - 1]
+            elif int(letter[0]) == 0 and len(letter) == 1:
+                result += ' '
+            else:
+                return None
+        else:
+            return None
+        print(result)
+            
+            
     return result
--- 3_functions/encode/encode.py
+++ 3_functions/encode/encode.py
@@ -1,4 +1,30 @@
+import numpy as np
 def encode_text(text: str) -> str | None:
     """Пишите ваш код здесь."""
     result = ""
+    # Получим таблицу кодировки из букв в клавиши телефона без ручного ввода
+    
+    letters = np.array([['а', 'б', 'в', 'г'], ['д', 'е', 'ж', 'з'], ['и', 'й', 'к', 'л'], ['м','н','о','п'], ['р','с','т','у'], ['ф','х','ц','ч'], ['ш','щ','ъ','ы'], ['ь','э','ю','я']])
+    special_symbols = np.array(['.', ',', '?', '!', ':', ';'])
+    
+    letters_table_encode = {}
+    
+    for i in range(0,8):
+        for j in range(0,4):
+            letters_table_encode[str(letters[i][j])] = (j+1)*str(i+2)
+            
+    for i in range(0,len(special_symbols)):
+        letters_table_encode[str(special_symbols[i])] = (i+1)*'1'
+
+    letters_table_encode[' '] = '0' #Таблица кодировки получена
+    # print(letters_table_encode)
+            
+    for symb in text:
+        # print(symb)
+        try:
+            result += letters_table_encode[symb]
+            result += ' '
+        except KeyError:
+            return None
+    result = result[0:len(result) - 1] #Убираем лишний пробел в конце
     return result
